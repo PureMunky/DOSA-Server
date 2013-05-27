3a96c8b9bdca62ab361759420b932753306353ee
Phil Corbett <corbett.phil@gmail.com>
2013-05-27 19:35:46 -0400

    create passdata object

---
+++ DOSA/Classes/PassData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DOSA
+{
+    [Serializable]
+    public class PassData
+    {
+        public string URI;
+        public object Payload;
+    }
+}
