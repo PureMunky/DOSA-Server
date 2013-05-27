64471d980009959431bc4d4a6223036c0c5ca6cc
Phil Corbett <corbett.phil@gmail.com>
2013-05-27 19:35:53 -0400

    create application object

---
+++ DOSA/Classes/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DOSA
+{
+    [Serializable]
+    public class Application
+    {
+        public int LocalID;
+        public string Name;
+        public string URIRoot;
+    }
+}
